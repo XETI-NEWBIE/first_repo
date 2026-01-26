7117291f570b04bed15d11a4c74d3edf12c5f390
홍채원 <sophia1919@naver.com>
2026-01-26 23:50:06 +0900

    algorithm

--- ws_4_a.py
+++
@@ -1,7 +0,0 @@
-# 아래에 코드를 작성하시오.
-from conf import settings
-from utils import create_url
-name = settings.NAME
-main_url = settings.MAIN_URL
-create_url1 = create_url.create_url(name,main_url)
-print(create_url1)
